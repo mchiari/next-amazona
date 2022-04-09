052a0ff158b75b8b950c500bbcd335b436909148
Matheus Chiari <mchiaridev@gmail.com>
2022-04-09 14:47:06 -0300

    implementing shopping cart screen and updating quantities

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -16,24 +16,30 @@ import Product from "../../models/Product";
 import db from "../../utils/db";
 import { Store } from "../../utils/Store";
 import useStyles from "../../utils/styles";
+import {useRouter} from "next/router";
 
 export default function ProductScreen(props) {
-    const {dispatch} = useContext(Store);
+    const {state, dispatch} = useContext(Store);
 	const { product } = props;
+	const router = useRouter();
 	const classes = useStyles();
 	if (!product) {
 		return <div>Product Not Found :(</div>;
 	}
 
 	const addToCartHandler = async () => {
+		const existItem = state.cart.cartItems.find(x=>x._id === product._id);
+		const quantity = existItem? existItem.quantity+1 : 1;
 		const { data } = await axios.get(`/api/products/${product._id}`);
-		if(data.countInStock <= 0) {
+		if(data.countInStock < quantity) {
             window.alert('Sorry. Product is out of stock.')
+			return;
         }
         dispatch({
 			type: "CART_ADD_ITEM",
-			payload: { ...product, quantity: 1 },
+			payload: { ...product, quantity: quantity },
 		});
+		router.push('/cart')
 	};
 
 	return (
