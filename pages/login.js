1049a28df2e9a72b7399b2d2ff4d84c879198acf
Matheus Chiari <mchiaridev@gmail.com>
2022-05-06 12:02:10 -0300

    implementing validation on register and login

--- pages/login.js
+++ pages/login.js
@@ -6,7 +6,7 @@ import {
 	Typography,
 	Link,
 } from "@material-ui/core";
-import React, { useState, useContext, useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import Layout from "../components/Layout";
 import useStyles from "../utils/styles";
 import NextLink from "next/link";
@@ -14,8 +14,16 @@ import axios from "axios";
 import { Store } from "../utils/Store";
 import { useRouter } from "next/router";
 import Cookies from "js-cookie";
+import { useForm, Controller } from "react-hook-form";
+import { useSnackbar } from "notistack";
 
 const Login = () => {
+	const {
+		handleSubmit,
+		control,
+		formState: { errors },
+	} = useForm();
+	const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 	const router = useRouter();
 	const { redirect } = router.query;
 	const { state, dispatch } = useContext(Store);
@@ -27,12 +35,10 @@ const Login = () => {
 		}
 	});
 
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
 	const classes = useStyles();
 
-	const submitHandler = async (e) => {
-		e.preventDefault();
+	const submitHandler = async ({ email, password }) => {
+		closeSnackbar();
 		try {
 			const { data } = await axios.post("/api/users/login", {
 				email,
@@ -41,37 +47,82 @@ const Login = () => {
 			dispatch({ type: "USER-LOGIN", payload: data });
 			Cookies.set("userInfo", JSON.stringify(data));
 			router.push(redirect || "/");
-			router.reload(window.location.pathname)
+			router.reload(window.location.pathname);
 		} catch (err) {
-			alert(err.response.data ? err.response.data.message : err.message);
+			enqueueSnackbar(
+				err.response.data ? err.response.data.message : err.message,
+				{ variant: "error" }
+			);
 		}
 	};
 	return (
 		<Layout title={"Login"}>
-			<form className={classes.form} onSubmit={submitHandler}>
+			<form
+				className={classes.form}
+				onSubmit={handleSubmit(submitHandler)}
+			>
 				<Typography component='h1' variant='h1'>
 					Login
 				</Typography>
 				<List>
 					<ListItem>
-						<TextField
-							variant='outlined'
-							fullWidth
-							id='email'
-							label='Email'
-							inputProps={{ type: "email" }}
-							onChange={(e) => setEmail(e.target.value)}
-						></TextField>
+						<Controller
+							name='email'
+							control={control}
+							defaultValue=''
+							rules={{
+								required: true,
+								pattern:
+									/^[a-z0-9._%=-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+							}}
+							render={({ field }) => (
+								<TextField
+									variant='outlined'
+									fullWidth
+									id='email'
+									label='Email'
+									inputProps={{ type: "email" }}
+									error={Boolean(errors.email)}
+									helperText={
+										errors.email
+											? errors.email.type === "pattern"
+												? "Email is not valid"
+												: "Email is required"
+											: ""
+									}
+									{...field}
+								></TextField>
+							)}
+						></Controller>
 					</ListItem>
 					<ListItem>
-						<TextField
-							variant='outlined'
-							fullWidth
-							id='password'
-							label='Password'
-							inputProps={{ type: "password" }}
-							onChange={(e) => setPassword(e.target.value)}
-						></TextField>
+						<Controller
+							name='password'
+							control={control}
+							defaultValue=''
+							rules={{
+								required: true,
+								minLength: 6,
+							}}
+							render={({ field }) => (
+								<TextField
+									variant='outlined'
+									fullWidth
+									id='password'
+									label='Password'
+									inputProps={{ type: "password" }}
+									error={Boolean(errors.password)}
+									helperText={
+										errors.password
+											? errors.password.type === "pattern"
+												? "Password is length is more than 5"
+												: "Password is required"
+											: ""
+									}
+									{...field}
+								></TextField>
+							)}
+						></Controller>
 					</ListItem>
 					<ListItem>
 						<Button
@@ -87,7 +138,10 @@ const Login = () => {
 						<Typography component='p'>
 							Don&apos;t have an account? &nbsp;
 						</Typography>{" "}
-						<NextLink href={`/register?redirect=${redirect || '/'}`} passHref>
+						<NextLink
+							href={`/register?redirect=${redirect || "/"}`}
+							passHref
+						>
 							<Link>Register</Link>
 						</NextLink>
 					</ListItem>
