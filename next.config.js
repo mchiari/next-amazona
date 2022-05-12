3e6477abfa4602309ae2ea0783edac0a9aecfb3a
Matheus Chiari <mchiaridev@gmail.com>
2022-05-12 18:09:41 -0300

    this is getting in my nerves

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 
+const Dotenv = require('dotenv-webpack');
+
 const nextConfig = {
 	reactStrictMode: false,
+  plugins:[
+    new Dotenv()
+  ],
   webpack: (config, { isServer }) => {
     if (!isServer) {
         config.resolve.fallback = {
