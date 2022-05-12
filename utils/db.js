7b80cbf4d9aa1d21287b18df0719ce3406260081
Matheus Chiari <mchiaridev@gmail.com>
2022-05-12 18:35:19 -0300

    fix mongoose

--- utils/db.js
+++ utils/db.js
@@ -7,7 +7,7 @@ async function connect(){
     if(connection.isConnected) {
         console.log('already connected');
         return;
-    }
+    } 
     if(mongoose.connections.length > 0){
         connection.isConnected = mongoose.connections[0].readyState;
         if(connection.isConnected === 1){
